9239c6f622f7f81f1250e882ab6488d16bacb99c
Gabriela-Mozer <75142121+Gabriela-Mozer@users.noreply.github.com>
2022-09-05 12:10:25 +0200

    Add files via upload
    initial commit

---
+++ synersie/main.js
@@ -0,0 +1,90 @@
+const getData = () => {
+  const save = (data) => {
+    localStorage.setItem(data.id, JSON.stringify(data));
+  };
+
+  const main = () => {
+    const currentHost = window.location.host.toLowerCase();
+    let data;
+    switch (currentHost) {
+      case "mediaexpert.pl":
+      case "www.mediaexpert.pl":
+        data = fetchForMediaExpert();
+        break;
+      case "mediamarkt.pl":
+      case "www.mediamarkt.pl":
+        data = fetchForMediaMarkt();
+        break;
+      case "euro.com.pl":
+      case "www.euro.com.pl":
+        data = fetchForEuro();
+        break;
+      default:
+        throw new Error("Page unknown");
+    }
+    save(data);
+  };
+async  function fetchForMediaExpert() {
+    const name = document.querySelector(
+      ".name.is-title[data-v-16fb7132]"
+    ).innerText;
+    const id = document.querySelector(
+      ".id.is-regular[data-v-16fb7132]"
+    ).innerText;
+    const price = document.querySelector(".").innerText;
+    const url = window.location.href;
+    const rating =
+      document
+        .querySelector("div.product-rating[data-v-16fb7132] .star.icon")
+        .style.width.slice(0, -1) / 100;
+    const foto = await getB64Image( document.querySelector(
+      ".spark-image img[data-v-43312c86]"
+    ).src); //?
+    return { name, id, price, url, rating, foto };
+    // getB64Image()
+  }
+ async function fetchForMediaMarkt() {
+    const name = document.querySelector(".title").innerText;
+    const id = document.querySelector(".catalog").innerText;
+    const price = document.querySelector(".whole").innerText;
+    const url = window.location.href;
+    const rating =
+      document
+        .querySelector(".star.is-filled[data-v-62d0d4e6]")
+        .style.width.slice(0, -1) / 100;
+    const foto = await getB64Image(document.querySelector(".is-loaded").src); //?
+    return { name, id, price, url, rating, foto };
+    getB64Image()
+  } //https://mediamarkt.pl/konsole-i-gry/playstation-5/konsole-ps5
+ async function fetchForEuro() {
+    // const img = await getB64Image()
+    const name = document.querySelector("h1.product-name").innerText;
+    const id = document.querySelector(".selenium-product-code").innerText;
+    const price = document.querySelector(
+      ".price-tabs .product-price"
+    ).innerText;
+    const url = window.location.href;
+    const rating =
+      document.querySelector(".stars-rating>a>span").style.width.slice(0, -1) /
+      100;
+    const foto =await getB64Image( document.querySelector("#big-photo img").src);
+    return { name, id, price, url, rating, foto };
+   
+}
+  main();
+};
+getData();
+
+async function getB64Image(imgUrl) {
+  const blobImg = await imgUrl.then((resp) => resp.blob());
+
+ return await new Promise((resolve, reject) => {
+    const read =  new  FileReader();
+    read.onloadend = () => {
+      resolve(read.result);
+    };
+    read.readAsDataUrl(blobImg);
+    
+  });
+}
+
